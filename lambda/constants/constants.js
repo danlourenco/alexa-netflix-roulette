6317fabc65fb1395fca5fd711e1d6146b031968c
Dan Lourenco <danlourenco@gmail.com>
2017-04-22 16:53:49 -0700

    changes to intent schema, move logic to index.js

--- lambda/constants/constants.js
+++ lambda/constants/constants.js
@@ -1,17 +1,15 @@
 const constants = {
-  appId: 'amzn1.ask.skill.29a8021a-bd76-440a-b3bb-b3201bef7ac8',
+  appId: '',
 
-  dynamoDBTableName: 'DogLogUsers',
-  dynamoDBEventTableName: 'DogLogEvents',
   // Skill States
   states: {
     ONBOARDING: '',
     MAIN: '_MAIN'
   },
   phrases: {
-    RECORD_PET_EVENT_PROMPT: `What would you like to do? You can say: <w role="ivona:VB">record</w> pee, <w role="ivona:VB">record</w> poop,
-    <w role="ivona:VB">record</w> nap, or <w role="ivona:VB">record</w> meal.`
-  }
+
+  },
+  BASE_URL: `http://netflixroulette.net/api/api.php?`
 };
 
 module.exports = constants;
--- lambda/index.js
+++ lambda/index.js
@@ -3,22 +3,38 @@
 const Alexa = require('alexa-sdk');
 const constants = require('./constants/constants');
 
-// Handlers
-var onboardingStateHandlers = require('./handlers/onboardingStateHandlers');
-var mainStateHandlers = require('./handlers/mainStateHandlers');
-
 /* eslint-disable */
 exports.handler = function(event, context, callback){
   var alexa = Alexa.handler(event, context);
 
   alexa.appId = constants.appId;
 
-  alexa.dynamoDBTableName = constants.dynamoDBTableName;
   alexa.registerHandlers(
-    onboardingStateHandlers,
-    mainStateHandlers
+    handlers
   );
   alexa.execute();
 };
+
+const handlers = {
+  'LaunchRequest': function() {
+    // var petName = this.attributes['petName'];
+    // if (petName) {
+    //   this.emit(':ask', `Welcome back! Let's record some more data about ${petName}. ` +  GET_INTENT_REPROMPT, GET_INTENT_REPROMPT);
+    // } else {
+    //   this.handler.state = constants.states.ONBOARDING;
+    //   this.emitWithState('NewSession');
+    // }
+    this.emit(':ask', `Let's find you something great to watch on Netflix. But first, I'll need a little information. You can say: get movie by, and then the actor or actresses' name.  What would you like to do?`, `You can say: get movie by, and then the actor or actresses' name.`);
+
+  },
+
+  'GetMovieByActor': function () {
+    
+  },
+
+  'GetMovieByDirector': function () {
+
+  }
+}
 
 /* eslint-enable */
