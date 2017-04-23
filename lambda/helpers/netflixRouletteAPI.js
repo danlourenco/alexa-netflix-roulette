a822aa082e016d55ff8ca4b33f25e2eb9dc8496b
Dan Lourenco <danlourenco@gmail.com>
2017-04-22 18:40:13 -0700

    test

--- lambda/helpers/netflixRouletteAPI.js
+++ lambda/helpers/netflixRouletteAPI.js
@@ -2,17 +2,19 @@ const rp = require('request-promise');
 const querystring = require('querystring');
 const constants = ('../constants');
 module.exports = {
-  // TODO: parameterize actor
+
   GetMovieByActor: (actor) => {
     var uriEncodedActorName = querystring.escape(actor);
     console.log('actor:', uriEncodedActorName);
     return new Promise((resolve, reject) => {
-
-      rp(`${constants.baseUrl}actor=${uriEncodedActorName}`)
+      console.log('uriEncodedActorName', uriEncodedActorName);
+      rp('http://netflixroulette.net/api/api.php?actor=' + uriEncodedActorName)
       .then((response) => {
+        console.log('response: ', response);
         resolve(JSON.parse(response));
       })
       .catch((error) => {
+        console.log('error: ', error);
         reject('NetflixRoulette API error: ', error);
       });
     });
